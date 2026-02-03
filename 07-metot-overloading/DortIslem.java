554fcce5e5a424f28d87895c727d49782497c21c
Aysun <104984443+aysunak1@users.noreply.github.com>
2026-02-03 17:11:27 +0300

    Metot Overloading

---
+++ 07-metot-overloading/DortIslem.java
@@ -0,0 +1,18 @@
+public class DortIslem {
+
+    public DortIslem(){
+        System.out.println("Yapıcı blok çalıştı.");
+    }
+
+    public int topla(int sayi1,int sayi2){
+        return sayi1+sayi2;
+    }
+    public int topla(int...sayilar){
+        int toplam = 0;
+        for (int sayi:sayilar){
+            toplam+=sayi;
+        }
+        return toplam;
+    }
+
+}
---
+++ 07-metot-overloading/README.md
@@ -0,0 +1,13 @@
+# Metot Overloading
+
+Bu klasörde Java'da **metot overloading** kullanımı
+örneklenmiştir.
+
+## İçerik
+- Aynı isimli metotların farklı parametrelerle tanımlanması
+- Varargs kullanımı
+- Yapıcı metot (constructor) kullanımı
+
+## Amaç
+Metot overloading mantığını ve
+farklı parametre yapılarıyla metot kullanımını kavramak.
